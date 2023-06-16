e16a6bc3964b49c338802c40a10a3b3cf410e470
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-06-16 14:44:39 +0600

    feat: add ZIP tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+  const fileToDecompress = 'fileToCompress.txt';
+  const compressedFile = 'archive.gz';
+  const inputFile = path.join(folderPath, compressedFile);
+  const outputFile = path.join(folderPath, fileToDecompress);
+
+  try {
+    const readStream = fs.createReadStream(inputFile);
+    const writeStream = fs.createWriteStream(outputFile);
+    const unzipStream = zlib.createGunzip();
+
+    readStream.pipe(unzipStream).pipe(writeStream);
+
+    await new Promise((res, rej) => {
+      writeStream.on('finish', res);
+      writeStream.on('error', rej);
+    });
+
+    console.log('Decompression completed');
+  } catch (error) {
+    console.error('Decompression failed:', error);
+  }
 };
 
 await decompress();
