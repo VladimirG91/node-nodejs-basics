6c5a3a22bd562db470c5e5b77d48edec94700867
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-06-16 13:54:15 +0600

    feat: CLI tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let i = 0;
+
+  while (i < args.length) {
+    const argName = args[i].slice(2); // Remove the leading --
+    const argValue = args[i + 1];
+    console.log(`${argName} is ${argValue}`);
+    i += 2;
+  }
 };
 
 parseArgs();
