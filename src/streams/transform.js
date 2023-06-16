7ea3e51fb849aa65ef6934a52bd7d4494cb8c1a0
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-06-16 15:03:26 +0600

    feat: streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().split('').reverse().join('');
+        this.push(reversedChunk);
+        callback();
+      },
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+    console.log('Transformation completed');
+  } catch (error) {
+    console.error('Transformation failed:', error);
+  }
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+  const fileToWrite = 'fileToWrite.txt';
+  const outputFile = path.join(folderPath, fileToWrite);
+
+  try {
+    const writeStream = fs.createWriteStream(outputFile);
+    process.stdin.pipe(writeStream);
+    console.log('Writing completed');
+  } catch (error) {
+    console.error('Writing failed:', error);
+  }
 };
 
 await write();
