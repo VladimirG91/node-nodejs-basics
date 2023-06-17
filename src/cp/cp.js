decb30e5fb48085af78272e4a4154df62aa0fcad
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-06-17 20:12:42 +0600

    feat: add CP taska solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import cp from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const folderPath = path.join(__dirname, 'files');
+  const childFile = 'script.js';
+  const childPath = path.join(folderPath, childFile);
+
+  const childProcess = cp.fork(childPath, args, { silent: true });
+  pipeline(process.stdin, childProcess.stdin, () => {});
+  pipeline(childProcess.stdout, process.stdout, () => {});
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5]);
