d915a1ec9fc2ec0053fbf1a983cd604c6372192d
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-06-19 16:36:14 +0600

    fix: remove all files, to crate PR and merge it

--- src/cli/env.js
+++
@@ -1,9 +0,0 @@
-const parseEnv = () => {
-  for (const key in process.env) {
-    if (key.startsWith('RSS_')) {
-      console.log(`RSS_${key.slice(4)}=${process.env[key]}`);
-    }
-  }
-};
-
-parseEnv();
