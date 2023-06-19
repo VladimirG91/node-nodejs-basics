00ab9ebb3a73113bab50a09cd8be025aab550ed5
Vladimir Glazkov <vladimir.glazkov91@gmail.com>
2023-06-19 16:00:57 +0600

    feat: add branch for PR

--- src/fs/create.js
+++ src/fs/create.js
@@ -8,7 +8,6 @@ const __dirname = path.dirname(__filename);
 const create = async () => {
   const text = 'I am fresh and young';
   const filePath = path.resolve(__dirname, 'files', 'fresh.txt');
-
   try {
     await fs.promises.access(filePath, fs.constants.F_OK);
     throw new Error('FS operation failed');
